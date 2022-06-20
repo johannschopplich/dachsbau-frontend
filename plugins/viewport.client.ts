66afcac23ceb3db7258deda9317a3cd13de2ea5c
Johann Schopplich <mail@johannschopplich.com>
2022-06-20 14:51:32 +0200

    feat: vh client plugin

---
+++ plugins/viewport.client.ts
@@ -0,0 +1,9 @@
+export default defineNuxtPlugin(() => {
+  if (matchMedia('(hover: hover)').matches) return
+
+  // Set actual vh on mobile
+  document.documentElement.style.setProperty(
+    '--vh',
+    `${window.innerHeight * 0.01}px`
+  )
+})
