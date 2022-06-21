9deffa7ca9df5b40d7ba9f443fb3a9665b6cb4b4
Johann Schopplich <mail@johannschopplich.com>
2022-06-21 17:21:46 +0200

    fix: position fixed container scrolling

--- app/router.options.ts
+++ app/router.options.ts
@@ -2,6 +2,19 @@ import type { RouterConfig } from '@nuxt/schema'
 
 export default <RouterConfig>{
   scrollBehavior(to, from, savedPosition) {
+    const container =
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      document.querySelector<HTMLDivElement>('#scroll-container')!
+
+    // Timeout for page transition
+    setTimeout(() => {
+      if (savedPosition) {
+        container.scroll(savedPosition)
+      } else {
+        container.scroll(0, 0)
+      }
+    }, 150)
+
     if (savedPosition) {
       return savedPosition
     } else {
