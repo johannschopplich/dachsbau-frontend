7ca1db5ef6c84c451dca9bc07bf56199377212c2
Johann Schopplich <mail@johannschopplich.com>
2022-06-30 15:04:08 +0200

    refactor: fetch site data in client

---
+++ plugins/site.client.ts
@@ -0,0 +1,31 @@
+import { defineNuxtPlugin } from '#app'
+
+export default defineNuxtPlugin(async () => {
+  const site = useSite()
+
+  try {
+    const data = await $kql({
+      query: 'site',
+      select: {
+        title: true,
+        description: true,
+        children: {
+          query: 'site.children',
+          select: ['id', 'title', 'isListed'],
+        },
+        cover: {
+          query: 'site.content.cover.toFile',
+          select: ['id', 'filename', 'url', 'srcset'],
+        },
+        footer: {
+          query: 'site.footer.toPages',
+          select: ['id', 'title'],
+        },
+      },
+    })
+
+    site.value = data?.result || {}
+  } catch (e) {
+    console.error('Error loading site data:', e.message)
+  }
+})
