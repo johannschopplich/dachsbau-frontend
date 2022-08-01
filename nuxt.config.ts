a0661c9707cb4991b2a63de47b6005b428366e15
Johann Schopplich <mail@johannschopplich.com>
2022-08-01 20:30:26 +0200

    chore: try workaround for nuxt hydration issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,16 @@ export default defineNuxtConfig({
     },
   },
 
+  hooks: {
+    'vite:extendConfig'(config, { isServer }) {
+      if (isServer) {
+        // Workaround for Netlify issue
+        // https://github.com/nuxt/framework/issues/6204
+        config.build.rollupOptions.output.inlineDynamicImports = true
+      }
+    },
+  },
+
   typescript: {
     shim: false,
   },
