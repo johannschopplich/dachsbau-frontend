fad622722bbd66d571f98e77fc25f5c49d57d698
Johann Schopplich <mail@johannschopplich.com>
2022-08-25 13:19:46 +0200

    feat: backers page

--- pages/[...id].vue
+++ pages/[...id].vue
@@ -3,12 +3,19 @@ import type { KirbyQueryResponse } from '#nuxt-kql'
 import type { KirbyDefaultPage, KirbyDefaultPageQuery } from '~/types'
 
 const route = useRoute()
+const routeSelects: Record<string, Record<string, any>> = {
+  '/freundinnen': {
+    backers: 'page.backers.toStructure',
+  },
+}
+
 const select = {
   id: true,
   title: true,
   description: true,
   text: 'page.text.toBlocks',
   layouts: 'page.layout.toLayouts',
+  ...(routeSelects?.[route.path] ?? {}),
   images: {
     query: 'page.images',
     select: ['id', 'filename', 'url', 'srcset', 'alt'],
@@ -56,6 +63,37 @@ const hasLayouts = computed(() => !!page.value?.layouts?.length)
       :blocks="page?.text ?? []"
       class="prose text-secondary-900 font-serif md:text-xl md:font-350"
     />
+
+    <!-- Custom template logic -->
+    <div v-if="page.backers" class="grid gap-2 grid-cols-2 md:grid-cols-3">
+      <div
+        v-for="(item, index) in page.backers"
+        :key="index"
+        class="relative bg-secondary-100 p-3"
+      >
+        <div class="flex justify-center mb-2">
+          <img
+            v-if="item.logo?.length"
+            class="h-10"
+            :srcset="
+              page.images.find((i: any) => i.filename === item.logo?.[0])?.srcset
+            "
+            :alt="`Logo für ${item.title}`"
+          />
+          <div v-else class="h-10" />
+        </div>
+
+        <component
+          :is="item.website ? 'a' : 'p'"
+          :href="item.website"
+          class="text-secondary-800 text-xl font-heading-condensed"
+        >
+          <div class="absolute inset-0" />
+          {{ item.title }}
+        </component>
+        <p class="text-secondary-600 text-sm font-serif" v-html="item.text" />
+      </div>
+    </div>
   </div>
 </template>
 
