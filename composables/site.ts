a2f584993a1ce0c6579b8d33f201b908e9ddae4d
Johann Schopplich <mail@johannschopplich.com>
2022-10-20 08:32:36 +0200

    perf: don't hydrate pre-fetched site value

--- composables/site.ts
+++ composables/site.ts
@@ -1,6 +1,5 @@
 import { kirbySite } from '#build/kql'
-import type { KirbySite } from '#build/kql'
 
 export function useSite() {
-  return ref<Partial<KirbySite>>(kirbySite)
+  return ref(kirbySite)
 }
