e9a9b4141fbe29bb3e7c3f1c5240b97d3706649c
Johann Schopplich <mail@johannschopplich.com>
2023-06-07 19:09:58 +0200

    feat: remember scroll position in fixed container

---
+++ plugins/better-scroller.client.ts
@@ -0,0 +1,28 @@
+import { createRouterScroller } from 'vue-router-better-scroller'
+
+interface ScrollPositionCoordinates {
+  behavior?: ScrollOptions['behavior']
+  left?: number
+  top?: number
+}
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(
+    createRouterScroller({
+      selectors: {
+        // window: true,
+        '#scroll-container': ({ type, savedPosition }) =>
+          new Promise<ScrollPositionCoordinates>((resolve) => {
+            // Handle Suspense route resolution
+            nuxtApp.hooks.hookOnce('page:finish', () => {
+              if (type === 'push') {
+                resolve({ top: 0, left: 0 })
+              } else if (type === 'history') {
+                resolve(savedPosition || { top: 0, left: 0 })
+              }
+            })
+          }),
+      },
+    })
+  )
+})
