0b968fd51ab491fb67a0a3b43d291d36e953274c
Johann Schopplich <mail@johannschopplich.com>
2023-08-26 15:10:18 +0200

    refactor: use `useHead`

--- app.vue
+++ app.vue
@@ -3,8 +3,33 @@ import '~/assets/css/main.css'
 import '~/assets/css/components.css'
 import '~/assets/css/prose.css'
 
+import '~/assets/fonts/Fraunces.css'
+import '~/assets/fonts/Henrietta.css'
+import '~/assets/fonts/HenriettaCondensed.css'
+
 const appState = useAppState()
 const appContainer = useAppContainer()
+
+useHead({
+  htmlAttrs: {
+    class: 'min-w-[320px] bg-primary-700',
+    lang: 'de',
+  },
+  link: [
+    {
+      rel: 'icon',
+      href: '/favicon.png',
+      type: 'image/png',
+      sizes: '32x32',
+    },
+  ],
+  meta: [
+    {
+      name: 'theme-color',
+      content: '#3A455B',
+    },
+  ],
+})
 
 // Force scrolling inside container on mobile when clicking
 // on the (thin) frame border accidentally
@@ -14,49 +39,30 @@ onClickOutside(appContainer, () => {
 </script>
 
 <template>
-  <Html class="min-w-[320px] bg-primary-700" lang="de">
-    <Head>
-      <Link rel="icon" href="/favicon.png" type="image/png" sizes="32x32" />
-      <Link rel="stylesheet" href="~/assets/fonts/Fraunces.css" />
-      <Link rel="stylesheet" href="~/assets/fonts/Henrietta.css" />
-      <Link rel="stylesheet" href="~/assets/fonts/HenriettaCondensed.css" />
-      <Link
-        rel="preload"
-        href="~/assets/fonts/VCHenrietta-Regular.woff2"
-        as="font"
-        type="font/woff2"
-        crossorigin="anonymous"
-      />
-      <Meta name="theme-color" content="#3A455B" />
-    </Head>
+  <div
+    id="scroll-container"
+    ref="appContainer"
+    class="fixed inset-[var(--frame-width)] min-w-[320px] overflow-x-hidden overflow-y-scroll rounded-3xl bg-secondary-200 focus:outline-none"
+    tabindex="-1"
+  >
+    <NuxtLoadingIndicator color="#5A6B8D" />
 
-    <Body>
-      <div
-        id="scroll-container"
-        ref="appContainer"
-        class="fixed inset-[var(--frame-width)] min-w-[320px] overflow-x-hidden overflow-y-scroll rounded-3xl bg-secondary-200 focus:outline-none"
-        tabindex="-1"
-      >
-        <NuxtLoadingIndicator color="#5A6B8D" />
+    <div class="relative h-full">
+      <AppHeader />
 
-        <div class="relative h-full">
-          <AppHeader />
+      <main>
+        <NuxtPage />
+      </main>
 
-          <main>
-            <NuxtPage />
-          </main>
+      <AppFooter />
+    </div>
+  </div>
 
-          <AppFooter />
-        </div>
-      </div>
-
-      <div v-show="!appState.nav.isOpen" class="pointer-events-none">
-        <img
-          class="fixed bottom-0 right-0 w-[min(10rem,30vw)] md:bottom-1 md:right-1"
-          src="~/assets/img/dachs-im-rahmen-320.png"
-          alt=""
-        />
-      </div>
-    </Body>
-  </Html>
+  <div v-show="!appState.nav.isOpen" class="pointer-events-none">
+    <img
+      class="fixed bottom-0 right-0 w-[min(10rem,30vw)] md:bottom-1 md:right-1"
+      src="~/assets/img/dachs-im-rahmen-320.png"
+      alt=""
+    />
+  </div>
 </template>
